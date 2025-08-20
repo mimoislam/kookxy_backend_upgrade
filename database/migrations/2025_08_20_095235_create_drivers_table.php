4e9bd039498c36eac3ae127ff13ce49b92dab086
mimoislam <benoaie.islam@gmail.com>
2025-08-20 10:53:36 +0100

    first commit

---
+++ database/migrations/2025_08_20_095235_create_drivers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  public function up(): void
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone'); // required
+            $table->string('type_car')->nullable();
+            $table->decimal('earnings', 10, 2)->default(0);
+            $table->unsignedInteger('total_orders')->default(0);
+            $table->boolean('available')->default(true);
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->enum('status', ['active', 'inactive', 'banned'])->default('active');
+            $table->unsignedInteger('points')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drivers');
+    }
+};
